b6cfab15bb8cb24db7026ba6b35e733aba151b69
Toma Milićević <tomatomamilicevic@gmail.com>
2025-02-09 00:39:44 +0100

    new_orders-and-styling-v2

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
-function Header(){
-    return (
-    <header>
-        <Link to="/">Fast React Pizza Co.</Link>
+function Header() {
+  return (
+    <header className="flex justify-between border-b border-stone-500 bg-yellow-600 px-8 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
     </header>
-    );
+  );
 }
 
 export default Header;
